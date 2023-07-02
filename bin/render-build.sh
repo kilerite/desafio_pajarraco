d8c51fa33a97ed659dd5169b9ddbf13021f7cc16
Reyes_Moreno <r.moreno.ing@gmail.com>
2023-07-01 23:50:48 -0400

    commit de media noche

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:truncate_all       # Borra el contenido de todas las tablas en la base de datos
-bundle exec rake db:create     # Crea las tablas nuevamente
+bundle exec rake db:rollback    
 bundle exec rake db:migrate    # Ejecuta las migraciones pendientes
 bundle exec rake db:seed       # Carga los datos iniciales (seed) en la base de datos
