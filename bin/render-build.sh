10a6fb475a9ef769b1ac02d78ef9bae800963d7d
Reyes_Moreno <r.moreno.ing@gmail.com>
2023-07-01 21:44:02 -0400

    Otro

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,8 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:drop       # Elimina todas las tablas de la base de datos
+
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop       # Elimina todas las tablas de la base de datos
 bundle exec rake db:create     # Crea las tablas nuevamente
 bundle exec rake db:migrate    # Ejecuta las migraciones pendientes
 bundle exec rake db:seed       # Carga los datos iniciales (seed) en la base de datos
